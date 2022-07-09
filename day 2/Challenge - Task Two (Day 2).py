c0809a664a28aa84502a5a65ecad68ee6e678bae
Andenga <62437418+Andenga@users.noreply.github.com>
2022-07-09 20:42:41 +0300

    Update Challenge - Task Two (Day 2).py

--- day 2/Challenge - Task Two (Day 2).py
+++ day 2/Challenge - Task Two (Day 2).py
@@ -1,12 +1,34 @@
-
-#2. Write a python program that takes user inputs and determines what career the user should learn.
-#Requirements:
-#Store career options in a list or in variables.
-#Store career advices in a list.
-#Store career questions in a list.
-#Use conditional statements that determines the user selected choice.
-#When a wrong input is entered the program should print out an error message.
-#When all the questions are done the program should determine which career the user should 
-# venture in and program should terminate.
+#career_list
+Careers=["Medicine","Engineering","Hospitality","Business management","software developer","web developer"]
+Caree_advices=["For one to do any cause you have to have passion","The courses available for one to do them one has to have passed mathematics","For one to do medicine they have to have passed biology"]
+Career_questions=["Do you have passion?","did you pass mathematics?"]
+n=input("Which career do you wish to do?")
+If karia!==career:
+    Print("error")
+Else:
+    print(career_advice)
+    if karia=="medicine":
+        print ("answer yes or no")
+    X=input ("did you pass biology?")
+    X1=input ("did you pass mathematics?")
+    X2=input ("do you have a passion in medicine?")
+        if X&X1&X2 !=="yes","no":
+            print ("error")
+        elif X1&X2&X =="yes":
+            print ("you qualify to do medicine!!")
+        else:
+            print ("you do not qualify ")
+            end
+else:
+    Print ("answer yes or no")
+    Y=input ("Do you have passion in the selected career?")
+    Y1=input ("Did you pass mathematics?")
+    If Y&Y1 !== "yes","no":
+        print ("error")
+    elifY&Y1=="yes":
+        print ("You qualify to do",karia)
+    else:
+        print ("You do not qualify to do",karia)
+          end
 
 
