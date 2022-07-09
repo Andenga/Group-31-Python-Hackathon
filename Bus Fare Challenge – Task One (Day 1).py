e2232430db40f651fe2b934a60632d78088cbc0b
Andenga <62437418+Andenga@users.noreply.github.com>
2022-07-09 18:03:09 +0300

    Add files via upload

---
+++ Bus Fare Challenge – Task One (Day 1).py
@@ -0,0 +1,24 @@
+#Importing the relevant library
+from datetime import date
+
+#Getting today's date and storing it in a variable 'date'
+date = date.today()
+print("Date:", date)
+
+#Using today's date to get the name on the day of the week written in short form with the first letter capitalized.
+day = date.strftime('%a')
+print("Day:", day)
+
+#We use the weekday() method to get the name of the day of the week
+#x is a variable used to store the week no from 0 to 6 representing Monday to Friday
+x = date.today().weekday()
+
+#We use an if elif else statement to determine today's fare
+if x < 5:#0-4 is Mon to Fri
+    print ("Fare:", 100)
+elif x == 5: #5 Sat
+    print ("Fare:", 60)
+else:  #6 Sun
+    print ("Fare:", 80)
+
+
